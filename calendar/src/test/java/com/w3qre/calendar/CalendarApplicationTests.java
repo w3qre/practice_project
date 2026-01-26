0afa2a4e344115dce8133455516ed34b1a245e6e
sw970 <sw970@192.168.123.113>
2026-01-27 02:08:14 +0900

    Spring project initial commit

---
+++ calendar/src/test/java/com/w3qre/calendar/CalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w3qre.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
